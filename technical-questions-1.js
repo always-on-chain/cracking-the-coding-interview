166a6cc0a2c4d99b5668c2e46a2cf8f89bf0d348
waynekwest88@gmail.com <waynekwest88@gmail.com>
2018-04-19 17:21:30 -0700

    worked on a problem from Technical Question chapter

---
+++ technical-questions-1.js
@@ -0,0 +1,30 @@
+
+const getPerms = (s, b) => {
+  let sortedS = s.split('').sort().join('');
+  let count = 0;
+
+  for (var i = 0; i < b.length; i++) {
+    let chars = b.slice(i, i + s.length);
+    if (chars.split('').sort().join('') === sortedS) {
+      count++;
+    }
+  }
+
+  return count;
+};
+
+const test = () => {
+  let s = 'abbc';
+  let b = 'cbabadcbbabbcbabaabccbabc';
+  let expected = 7;
+  let result = getPerms(s, b);
+
+  if (expected === result) {
+    console.log('Passed!')
+  } else {
+    console.log('Failed: expected ' + expected + ' but got ' + result);
+  }
+}
+
+test();
+
